6b57f93d18c947a0df5c4a3ec1f98a21c238e910
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-04-02 22:29:27 +0300

    Data Beautify

--- scrapper.py
+++ scrapper.py
@@ -6,4 +6,16 @@ url = "https://coinmarketcap.com"
 html = requests.get(url).content
 soup = BeautifulSoup(html, "html.parser")
 
-print(soup)
+table = soup.find("table", {"class": "sc-beb003d5-3 ieTeVa cmc-table"})
+
+header = []
+rows = []
+for i, row in enumerate(table.find_all('tr')):
+    if i == 0:
+        header = [el.text.strip() for el in row.find_all('th')]
+    else:
+        rows.append([el.text.strip() for el in row.find_all('td')])
+
+print(header)
+for row in rows:
+    print(row)
